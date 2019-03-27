8e28b9bbbbe90657e3e94f7e2209f4db75d9fe31
malike <st.malike@gmail.com>
2019-03-27 23:59:14 +0000

    added bash scripts for aws specific features

--- src/main/java/st/malike/bot/service/SlackService.java
+++ src/main/java/st/malike/bot/service/SlackService.java
@@ -1,8 +1,15 @@
 package st.malike.bot.service;
 
+import java.io.IOException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SlackService {
+
+
+  public String sendToSlack() throws IOException {
+
+    return "";
+  }
 
 }
