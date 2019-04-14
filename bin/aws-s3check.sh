47d9cc5e2906386302676c9f419fbc6969f8eb78
malike <st.malike@gmail.com>
2019-04-14 23:58:08 +0000

    aws s3 audit script

--- bin/aws-s3check.sh
+++ bin/aws-s3check.sh
@@ -9,6 +9,13 @@ ARGV=($@)
 
 SLACK_CHANNEL=""
 FILTERED_BUCKETS=""
+USERS_COUNT=0
+BIN_DIRECTORY="$(
+    cd "$(dirname "$0")"
+    pwd -P
+)"
+S3_PUBLIC="${BIN_DIRECTORY}/S3_PUBLIC.txt"
+S3_PRIVATE="${BIN_DIRECTORY}/S3_PRIVATE.txt"
 
 function check_arguments() {
     if [[ $ARGC -ne 1 ]]; then
@@ -31,33 +38,42 @@ function help() {
 function main() {
 
     check_arguments
-    echo "Getting buckets ...."
     get_buckets
-    echo "Checking accessibility... "
     check_accessibility
-    echo "Uploading response to slack channel"
-    send_to_slack
+    # send_to_slack
 
     echo "COMPLETE!"
     exit 0
 }
 
 function check_accessibility_for_bucket() {
-
+    echo "Checking accessibility... "
 }
 
 function get_buckets() {
     echo "Getting buckets ...."
     FILTERED_BUCKETS=$(aws s3api list-buckets)
-    echo "Fetched $(echo FILTERED_BUCKETS | jq '.Buckets | length') buckets"
+    echo "Fetched $(echo $FILTERED_BUCKETS | jq '.Buckets | length') buckets"
 }
 
 function check_accessibility() {
-
+    echo "Checking accessibility... "
+    echo "" >S3_PRIVATE
+    echo "" >S3_PUBLIC
+    for BUCKETS in $(echo "${FILTERED_BUCKETS}" | jq -c '.Buckets[].Name'); do
+        BUCKET_PERMISSION=$(aws s3api get-bucket-acl --bucket $BUCKETS)
+        # echo "MFA for $AWS_USERAME is $MFA_DEVICE_COUNT"
+        if [[ $MFA_DEVICE_COUNT -lt 1 ]]; then
+            echo "$AWS_USERAME" >>USERS_NO_MFA_FILE
+        else
+            echo "$AWS_USERAME" >>USERS_MFA_FILE
+        fi
+    done
 }
 
 function send_to_slack() {
-    $(slack chat send --text $FILTERED_BUCKETS --channel "#"$SLACK_CHANNEL)
+    echo "Uploading response to slack channel"
+    # $(slack chat send --text $FILTERED_BUCKETS --channel "#"$SLACK_CHANNEL)
 }
 
 main
